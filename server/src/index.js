b155a02c46b7f76ec7be8f7e196b0179328f52db
skillswapskill <skillswapskill@gmail.com>
2025-08-09 01:42:14 +0530

    link change

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 // Dynamic CORS Configuration
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://skillsswap.onrender.com'
+  'https://skillswappt.onrender.com/'
 ];
 
 const corsOptions = {
