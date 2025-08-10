6ba26846ad7b0b0ac14812e9268a412c056c564d
skillswapskill <skillswapskill@gmail.com>
2025-08-10 12:04:00 +0530

    leave meeting btn remove

--- client/src/components/JoinRoom.jsx
+++ client/src/components/JoinRoom.jsx
@@ -105,7 +105,7 @@ const JoinRoom = () => {
   return (
     <div className="meeting-page">
       {/* Meeting Header - Optional overlay */}
-      <div className="absolute bottom-6 left-170 z-50 bg-black bg-opacity-50 text-white px-4 py-2 rounded-lg">
+      {/* <div className="absolute bottom-6 left-90  bg-black bg-opacity-50 text-white px-4 py-2 rounded-lg">
         <h3 className="font-semibold">{sessionData?.skill}</h3>
         <p className="text-sm opacity-75">
           Teacher: {sessionData?.teacher?.name || 'Unknown'}
@@ -116,7 +116,7 @@ const JoinRoom = () => {
         >
           Leave Meeting
         </button>
-      </div>
+      </div> */}
 
       {/* Video Call Component */}
       <VideoCall 
